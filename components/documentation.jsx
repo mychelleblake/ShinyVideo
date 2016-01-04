d2c8d3b88cd3e702aedaf5d0a4875c37924a6def
Mychelle Blake <mychelle.blake@gmail.com>
2016-01-04 09:56:32 -0800

    adds film image to documentation page

--- components/documentation.jsx
+++ components/documentation.jsx
@@ -15,11 +15,15 @@ var Documentation = React.createClass({
 						<ul>
 							<li className={this.state.activePane === 1 ? "active": ""}>
 						        <h3 onClick={()=>this.setState({activePane:1})}>Information for Developers</h3>
-								<div className="libraryText">shinyVideo is a library that allows comments to be timed to a video and played back in sync. It combines HTML, CSS3, <a href="http://www.popcornjs.org" target="_blank">Popcorn.js</a> (a media player) and <a href="https://facebook.github.io/react/" target="_blank">React.js</a>.</div>
+								<div className="libraryText">shinyVideo is a library that allows comments to be timed to a video and played back in sync. It combines HTML, CSS3, <a href="http://www.popcornjs.org" target="_blank">Popcorn.js</a> (a media player) and <a href="https://facebook.github.io/react/" target="_blank">React.js</a>.
+								<img src="/assets/film.png"/>
+								</div>
 							</li>
 							<li className={this.state.activePane === 2 ? "active": ""}>
 						        <h3 onClick={()=>this.setState({activePane:2})}>Popcorn.js</h3>
-								<div className="libraryText">Documentation for Popcorn.js can be found <a href="http://popcornjs.org/documentation" target="_blank">here</a> and the API reference is <a href="http://popcornjs.org/popcorn-docs/" target="_blank">here</a>.</div>
+								<div className="libraryText">Documentation for Popcorn.js can be found <a href="http://popcornjs.org/documentation" target="_blank">here</a> and the API reference is <a href="http://popcornjs.org/popcorn-docs/" target="_blank">here</a>.
+								<img src="/assets/film.png"/>
+								</div>
 							</li>
 							<li className={this.state.activePane === 3 ? "active": ""}>
 						        <h3 onClick={()=>this.setState({activePane:3})}>Code</h3>
@@ -29,13 +33,15 @@ var Documentation = React.createClass({
 										<li><a href="https://github.com/mychelleblake/ShinyVideo">Github Repository</a></li>
 										<li><a href="https://github.com/mychelleblake/ShinyVideo/blob/master/README.md">README.md file</a></li>
 									</ul>
+								<img src="/assets/film.png"/>
 								</div>
 							</li>
 							<li className={this.state.activePane === 4 ? "active": ""}>
 						        <h3 onClick={()=>this.setState({activePane:4})}>Contact</h3>
-								<div className="libraryText">For further information, contact Mychelle Blake at <a href="mailto:mychelle.blake@gmail.com">mychelle.blake@gmail.com</a>, <a href="http://www.twitter.com/mychelleblake" target="_blank">@mychelleblake</a> or <a href="http://www.mychelleblake.com" target="_blank">www.mychelleblake.com.</a></div>
+								<div className="libraryText">For further information, contact Mychelle Blake at <a href="mailto:mychelle.blake@gmail.com">mychelle.blake@gmail.com</a>, <a href="http://www.twitter.com/mychelleblake" target="_blank">@mychelleblake</a> or <a href="http://www.mychelleblake.com" target="_blank">www.mychelleblake.com.</a><img src="/assets/film.png"/></div>
 							</li>
 						</ul>
+						
 						</div>
 				</div>
 			</div>
