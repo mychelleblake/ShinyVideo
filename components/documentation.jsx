4ca2408b9a29c06b6462052b5634cc8280059bb1
Mychelle Blake <mychelle.blake@gmail.com>
2016-01-05 21:16:50 -0800

    adds more text and styling to documentation

--- components/documentation.jsx
+++ components/documentation.jsx
@@ -20,7 +20,13 @@ var Documentation = React.createClass({
 
 									<p>shinyVideo.js combines HTML, CSS3, <a href="http://www.popcornjs.org/" target="_blank">Popcorn.js</a> (a media player) and <a href="https://facebook.github.io/react/" target="_blank">React.js</a>.</p>
 
-									<p>shinyVideo.js was created as part of a class final project in the Front End Engineering program at the Iron Yard Las Vegas.</p><br/><br/><p className="rightImg"><img src="/assets/film.png"/></p>
+									<p>shinyVideo.js was created as part of a class final project in the Front End Engineering program at the Iron Yard Las Vegas.</p>
+
+									<p>The information for the library is available for download at this<a href="https://github.com/mychelleblake/ShinyVideo" target="_blank">Github Repository</a>.</p>
+
+									<p>For further information, contact Mychelle Blake at <a href="mailto:mychelle.blake@gmail.com">mychelle.blake@gmail.com</a>, <a href="http://www.twitter.com/mychelleblake" target="_blank">@mychelleblake</a> or <a href="http://www.mychelleblake.com" target="_blank">www.mychelleblake.com.</a></p>
+
+									<br/><p className="rightImg"><img src="/assets/film.png"/></p>
 								</div>
 							</li>
 							<li className={this.state.activePane === 2 ? "active": ""}>
@@ -33,15 +39,15 @@ var Documentation = React.createClass({
 										<li>You will also need a place to host your videos. The shinyVideo.js site hosts video files (mp4) on the site and all other information is accessed from an online database site <a href="http://www.parse.com" target="_blank">(parse.com)</a>.</li><br/>
 										<li>The ability to save and update comments will require a backend, either one that you specifically provide or a third-party site such as Parse.</li>
 									</ul>
-								<p className="rightImg"><img src="/assets/film.png"/></p>
+								<br/><br/><br/><p className="rightImg"><img src="/assets/film.png"/></p>
 								</div>
 							</li>
 							<li className={this.state.activePane === 3 ? "active": ""}>
 						        <h3 onClick={()=>this.setState({activePane:3})}>What type of data can be used?</h3>
-								<div className="libraryText2"><p>shinyVideo.js is configured to take data in JSON form. (Screenshot: Example of Parse Database)</p>
+								<div className="libraryText2"><p>shinyVideo.js is configured to take data in JSON form. (Screenshot: Example of Parse Database)</p><br/>
 								<img src="/assets/parseshot.jpg"/>
 
-								<p className="rightImg1"><img src="/assets/film.png"/></p>
+								<br/><br/><br/><br/><p className="rightImg1"><img src="/assets/film.png"/></p>
 								</div>
 							</li>
 							<li className={this.state.activePane === 4 ? "active": ""}>
@@ -49,7 +55,7 @@ var Documentation = React.createClass({
 								<div className="libraryText">
 									<p>Visit the Video Menu page on the shinyVideo.js site. A wide variety of videos are provided for examples. Videos with “EXAMPLE” in the description already have comments added. If you click on a thumbnail, you will be taken to the Video Player page.  For EXAMPLE videos, press play on the video player and you will see the comments scroll along the box on the right. For other videos, you can try out the process by typing comments in the Name and Comments boxes underneath the video player (and don’t forget to hit submit!) Press play on the video to replay and you will see the comments scroll in the box on the right (please note, you might need to refresh the screen and/or wait for a few seconds of delay as the data comes back to the site from Parse.)</p>
 
-										<p>If you wish to save or print the transcript, press the Print button underneath the scrolling comment box which will take you to a print window.  </p><br/><p className="rightImg"><img src="/assets/film.png"/></p></div>
+										<p>If you wish to save or print the transcript, press the Print button underneath the scrolling comment box which will take you to a print window.  </p><br/><br/><br/><br/><p className="rightImg"><img src="/assets/film.png"/></p></div>
 							</li>
 
 							<li className={this.state.activePane === 5 ? "active": ""}>
@@ -59,28 +65,47 @@ var Documentation = React.createClass({
 
 							<li className={this.state.activePane === 6 ? "active": ""}>
 						        <h3 onClick={()=>this.setState({activePane:6})}>What dependencies does shinyVideo.js use?</h3>
-								<div className="libraryText">shinyVideo.js uses the following dependencies installed via <a href="http://npmjs.com" target="_blank">npm</a>:
-									<ul>
-										<li>Backbone</li>
-										<li>CSS-loader</li>
-										<li>JSON-loader</li>
-										<li>JSX-loader</li>
-										<li>React</li>
-										<li>React-dom</li>
-										<li>React-hot-loader</li>
-										<li>React-router</li>
-										<li>Style-loader</li>
-										<li>Underscore</li>
-										<li>Webpack</li>
-										<li>Webpack-dev-server</li>										
+								<div className="libraryText">shinyVideo.js uses the following dependencies installed via <a href="http://npmjs.com" target="_blank">npm</a>:<br/><br/>
+									<ul className="docUL">
+										<li>Backbone</li><br/>
+										<li>CSS-loader</li><br/>
+										<li>JSON-loader</li><br/>
+										<li>JSX-loader</li><br/>
+										<li>React, React-dom and React-router</li><br/>
+										<li>Style-loader</li><br/>
+										<li>Underscore</li><br/>
+										<li>Webpack</li><br/>										
 									</ul>
-								<p className="rightImg"><img src="/assets/film.png"/></p></div>
+								<img className="imageFtRgt" src="/assets/film.png"/></div>
 							</li>
 
 							<li className={this.state.activePane === 7 ? "active": ""}>
-						        <h3 onClick={()=>this.setState({activePane:7})}>Contact</h3>
-								<div className="libraryText">For further information, contact Mychelle Blake at <a href="mailto:mychelle.blake@gmail.com">mychelle.blake@gmail.com</a>, <a href="http://www.twitter.com/mychelleblake" target="_blank">@mychelleblake</a> or <a href="http://www.mychelleblake.com" target="_blank">www.mychelleblake.com.</a><br/><br/><br/><p className="rightImg"><img src="/assets/film.png"/></p></div>
+						        <h3 onClick={()=>this.setState({activePane:7})}>What is the file structure available on Github?</h3>
+								<div className="libraryText">
+
+<p id="sourceCode">
+&nbsp;&nbsp;|-- components<br/>
+&nbsp;&nbsp;|------ documentation.jsx (this is for information only, not required for your project files)<br/>
+&nbsp;&nbsp;|------ homeView.jsx<br/>
+&nbsp;&nbsp;|------ menuView.jsx<br/>
+&nbsp;&nbsp;|------ nav.jsx<br/>
+&nbsp;&nbsp;|------ videoPlayer.jsx<br/>
+&nbsp;&nbsp;|-- css<br/>
+&nbsp;&nbsp;|------ main.css (covers css for all components except videoPlayer.jsx)<br/>
+&nbsp;&nbsp;|------ video.css (covers css for videoPlayer.jsx component)<br/>
+&nbsp;&nbsp;|-- js<br/>
+&nbsp;&nbsp;|------ backbone-parse.js	(update/use/delete based on your backend configuration)<br/>
+&nbsp;&nbsp;|------ main.jsx<br/>
+&nbsp;&nbsp;|------ parse-credentials.json (update/use/delete  based on your backend configuration)<br/>
+&nbsp;&nbsp;|------ routes.jsx<br/>
+&nbsp;&nbsp;index.html<br/>
+&nbsp;&nbsp;package.json<br/>
+&nbsp;&nbsp;README.md<br/>
+
+<img className="imageFtRgt" src="/assets/film.png"/></p></div>
 							</li>
+
+						
 						</ul>
 
 						</div>
