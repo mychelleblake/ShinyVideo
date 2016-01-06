1e6ee85730dd7655702b08cc1bffe42c7a00cfc6
Mychelle Blake <mychelle.blake@gmail.com>
2016-01-05 21:32:05 -0800

    adds more styling and final text to documentation

--- components/documentation.jsx
+++ components/documentation.jsx
@@ -22,7 +22,7 @@ var Documentation = React.createClass({
 
 									<p>shinyVideo.js was created as part of a class final project in the Front End Engineering program at the Iron Yard Las Vegas.</p>
 
-									<p>The information for the library is available for download at this<a href="https://github.com/mychelleblake/ShinyVideo" target="_blank">Github Repository</a>.</p>
+									<p>The information for the library is available for download at this &nbsp;<a href="https://github.com/mychelleblake/ShinyVideo" target="_blank">Github Repository</a>.</p>
 
 									<p>For further information, contact Mychelle Blake at <a href="mailto:mychelle.blake@gmail.com">mychelle.blake@gmail.com</a>, <a href="http://www.twitter.com/mychelleblake" target="_blank">@mychelleblake</a> or <a href="http://www.mychelleblake.com" target="_blank">www.mychelleblake.com.</a></p>
 
@@ -82,29 +82,40 @@ var Documentation = React.createClass({
 							<li className={this.state.activePane === 7 ? "active": ""}>
 						        <h3 onClick={()=>this.setState({activePane:7})}>What is the file structure available on Github?</h3>
 								<div className="libraryText">
-
-<p id="sourceCode">
-&nbsp;&nbsp;|-- components<br/>
-&nbsp;&nbsp;|------ documentation.jsx (this is for information only, not required for your project files)<br/>
-&nbsp;&nbsp;|------ homeView.jsx<br/>
-&nbsp;&nbsp;|------ menuView.jsx<br/>
-&nbsp;&nbsp;|------ nav.jsx<br/>
-&nbsp;&nbsp;|------ videoPlayer.jsx<br/>
-&nbsp;&nbsp;|-- css<br/>
-&nbsp;&nbsp;|------ main.css (covers css for all components except videoPlayer.jsx)<br/>
-&nbsp;&nbsp;|------ video.css (covers css for videoPlayer.jsx component)<br/>
-&nbsp;&nbsp;|-- js<br/>
-&nbsp;&nbsp;|------ backbone-parse.js	(update/use/delete based on your backend configuration)<br/>
-&nbsp;&nbsp;|------ main.jsx<br/>
-&nbsp;&nbsp;|------ parse-credentials.json (update/use/delete  based on your backend configuration)<br/>
-&nbsp;&nbsp;|------ routes.jsx<br/>
-&nbsp;&nbsp;index.html<br/>
-&nbsp;&nbsp;package.json<br/>
-&nbsp;&nbsp;README.md<br/>
-
-<img className="imageFtRgt" src="/assets/film.png"/></p></div>
+									<p id="sourceCode">
+									&nbsp;&nbsp;|-- components<br/>
+									&nbsp;&nbsp;|------ documentation.jsx (this is for information only, not required for your project files)<br/>
+									&nbsp;&nbsp;|------ homeView.jsx<br/>
+									&nbsp;&nbsp;|------ menuView.jsx<br/>
+									&nbsp;&nbsp;|------ nav.jsx<br/>
+									&nbsp;&nbsp;|------ videoPlayer.jsx<br/>
+									&nbsp;&nbsp;|-- css<br/>
+									&nbsp;&nbsp;|------ main.css (covers css for all components except videoPlayer.jsx)<br/>
+									&nbsp;&nbsp;|------ video.css (covers css for videoPlayer.jsx component)<br/>
+									&nbsp;&nbsp;|-- js<br/>
+									&nbsp;&nbsp;|------ backbone-parse.js	(update/use/delete based on your backend configuration)<br/>
+									&nbsp;&nbsp;|------ main.jsx<br/>
+									&nbsp;&nbsp;|------ parse-credentials.json (update/use/delete  based on your backend configuration)<br/>
+									&nbsp;&nbsp;|------ routes.jsx<br/>
+									&nbsp;&nbsp;index.html<br/>
+									&nbsp;&nbsp;package.json<br/>
+									&nbsp;&nbsp;README.md<br/>
+									<img className="imageFtRgt" src="/assets/film.png"/></p></div>
 							</li>
 
+							<li className={this.state.activePane === 8 ? "active": ""}>
+						        <h3 onClick={()=>this.setState({activePane:8})}>Can I customize shinyVideo.js?</h3>
+								<div className="libraryText"> Of course! All of the jsx files can be customized to your site and all code in the jsx files is commented to assist you with implementation. The component files cover:<br/><br/>
+									<ul>
+										<li><span className="jsxTitle">homeView.jsx </span>- the home page for this site - this does not need to be included in your project.</li><br/>
+										<li><span className="jsxTitle">menuView.jsx </span>- this is the “access point” for the videoPlayer page with examples of videos you can try to see shinyVideo.js in action. It is not required for your project but you will need to create a component that provides a method for videos to load to the player, either as individual pages or as a menu.</li><br/>
+										<li><span className="jsxTitle">nav.jsx </span>- this is the navigation for the shinyVideo.js site and does not need to be included.</li><br/>
+										<li><span className="jsxTitle">videoPlayer.jsx</span> - this is the main component file you will need. Each of the components can be moved and styled as you wish on your video page. See comments in the code for detailed information on each section.</li><br/>
+									</ul>
+										Two other important jsx files you will need in your js directory are <span className="jsxTitle">main.jsx</span> and <span className="jsxTitle">routes.jsx</span>. You will either need to implement both files or update them if you already have a main entry point and routing file created. 
+									<p className="rightImg"><img src="/assets/film.png"/></p>
+								</div>
+							</li>
 						
 						</ul>
 
